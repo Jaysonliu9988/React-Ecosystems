035216d645f4171d6d64f37066b8367cd89927c1
Jaysonliu <leobest9688@gmail.com>
2020-10-10 23:59:25 +0800

    fix bugs

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -5,7 +5,7 @@ const TodoListItem = ({ todo }) => (
   <div className="todo-item-container">
     <h3>{todo.text}</h3>
     <div className="buttons-containe">
-      <button className="compelted-button">Mark As Completed</button>
+      <button className="completed-button">Mark As Completed</button>
       <button className="remove-button">Remove</button>
     </div>
   </div>
